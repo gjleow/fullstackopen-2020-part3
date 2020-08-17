b35c900c15302c0be494d14062d26d081cdaf2aa
gj <gleow3@gatech.edu>
2020-08-17 22:32:21 +0100

    [GJ] Complete exercise 3.8 phonebook backend

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+morgan.token("res-body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :res-body"
+  )
+);
 
 let persons = [
   {
